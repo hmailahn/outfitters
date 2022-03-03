ff71ac69416856d42a62484718ebeb4eecb4616e
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-03-03 15:55:48 -0600

    started working on post-routes for chest and ran into issues. Need to talk about wardrobe and if it's necessary

--- controllers/api/chestwear-routes.js
+++ controllers/api/chestwear-routes.js
@@ -11,4 +11,48 @@ router.get('/', (req, res) => {
       });
   });
 
+  router.get('/:id', (req, res) => {
+    Chestwear.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'description',
+        'wardrobe_id'
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbChestwearData => {
+        if (!dbChestwearData) {
+          res.status(404).json({ message: 'No item found with this id' });
+          return;
+        }
+        res.json(dbChestwearData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.post('/', (req, res) => {
+  ///add in withAuth later
+      Chestwear.create({
+        description: req.body.description,
+        wardrobe_id: req.body.wardrobe_id
+      })
+        .then(dbChestwearData => res.json(dbChestwearData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    
+  });
+
   module.exports = router;
--- controllers/api/wardrobe-routes.js
+++ controllers/api/wardrobe-routes.js
@@ -11,5 +11,6 @@ router.get('/', (req, res) => {
       });
   });
 
+  
 
 module.exports = router;
