845a74efde240a1f51f59eaf91ebd3d33fb75873
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-03-04 12:42:42 -0600

    added seeds to wardrobe and clothing for testing. Works by running in terminal 'npm run seed'

--- seeds/Clothingseed.js
+++ seeds/Clothingseed.js
@@ -1,7 +1,39 @@
-const { Clothing } = require("../models/Clothing")
+const { Clothing } = require("../models/");
 
 const clothingData = [
     {
-        
+        description: "pink",
+        type: "shirt",
+        wardrobe_id: 1
+    },
+    {
+        description: "purple",
+        type: "pants",
+        wardrobe_id: 1
+    },
+    {
+        description: "black",
+        type: "shoes",
+        wardrobe_id: 1
+    },
+    {
+        description: "blue flowers",
+        type: "shirt",
+        wardrobe_id: 1
+    },
+    {
+        description: "gray sweats",
+        type: "pants",
+        wardrobe_id: 1
+    },
+    {
+        description: "vans",
+        type: "shoes",
+        wardrobe_id: 1
     }
-]
+];
+
+const seedClothing = () => Clothing.bulkCreate(clothingData);
+
+module.exports = seedClothing;
+
--- seeds/Userseed.js
+++ seeds/Userseed.js
@@ -1,13 +1,13 @@
-const User = require('../models/User')
+const User = require('../models/User');
 
 const userData = [
     {
         username: "test",
         email: "test@gmail.com",
         password: "12345678"
-    },
+    }
 ]
 
-const seedUsers = () => User.bulkCreate(userData)
+const seedUsers = () => User.bulkCreate(userData);
 
 module.exports = seedUsers
