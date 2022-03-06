0693dd1506ded2c1263cda596dc126f784a5145e
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-03-06 13:36:41 -0600

    fixed issue with seeds. They had user_id when it should have been wardrobe_id in seeds

--- controllers/api/clothing-routes.js
+++ controllers/api/clothing-routes.js
@@ -29,7 +29,7 @@ router.get('/chestwear', (req, res) => {
       attributes:[
         'id',
         // [sequelize.literal('(SELECT description FROM clothing WHERE type = "chestwear")'), 'chestwear_description'],
-        [sequelize.literal('(SELECT description FROM clothing WHERE type = (SELECT description FROM type WHERE type = "chestware"))'), 'chestwear_description']
+        [sequelize.literal('(SELECT description FROM clothing WHERE type = (SELECT description FROM clothing WHERE type = "chestware"))'), 'chestwear_description']
       ]
     })
     .then(dbClothingData => res.json(dbClothingData))
--- seeds/Clothingseed.js
+++ seeds/Clothingseed.js
@@ -4,32 +4,32 @@ const clothingData = [
     {
         description: "pink",
         type: "chestwear",
-        user_id: 1
+        wardrobe_id: 1
     },
     {
         description: "purple",
         type: "legwear",
-        user_id: 1
+        wardrobe_id: 1
     },
     {
         description: "black",
         type: "footwear",
-        user_id: 1
+        wardrobe_id: 1
     },
     {
         description: "blue flowers",
         type: "chestwear",
-        user_id: 1
+        wardrobe_id: 1
     },
     {
         description: "gray sweats",
         type: "legwear",
-        user_id: 1
+        wardrobe_id: 1
     },
     {
         description: "vans",
         type: "footwear",
-        user_id: 1
+        wardrobe_id: 1
     }
 ];
 
