21ecc5753f9d8fc844224c053762e28dbb64d2ac
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-03-08 19:49:39 -0600

    adding back in the logout and login script to main handlebars page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -42,8 +42,23 @@ router.get("/", (req, res) => {
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
+        const validPassword = dbUserData.checkPassword(req.body.password);
+      
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
       });
     });
+ });
+    
 
   router.post('/login', (req, res) => {
     User.findOne({
