35fa6680977450c612beed575613848f561eaba3
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-03-08 19:22:55 -0600

    restored sessions in home-routes.js. Now user will be redirected to login page if they are not logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 
 //homepage, user can click options to add to wardrobe or add clothing
 router.get("/", (req, res) => {
-  // if (!req.session.loggedIn) {
-//   res.redirect("/login");
-//   return;
-  // }
+  if (!req.session.loggedIn) {
+  res.redirect("/login");
+  return;
+  }
 res.render('homepage');
   //buttons on homepage to add clothing or view wardrobe
 });
@@ -13,28 +13,32 @@ res.render('homepage');
 //log in route. If user is loginned, they will be redirected to the home page /
 //if user is not logged in they will need to log in to go to their homepage
 router.get("/login", (req, res) => {
-  // // if (req.session.loggedIn) {
-  //   res.redirect('/');
-  //   return;
-  // // }
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
   res.render("login");
   
 });
 
+//add clothes
+//if user is not logged in, they will be redirected to login page
 router.get("/addclothes", (req, res) => {
-//  if(!req.session.loggedIn) {
-//      res.redirect("/login");
-//      return;
-//  }
+ if(!req.session.loggedIn) {
+     res.redirect("/login");
+     return;
+ }
   res.render('add-clothes');
 });
 
+//view clothes
+//if user is not logged in, they will be redirected to login page
 router.get("/viewclothes", (req, res) => {
-    // if(!req.session.loggedIn) {
-    //     res.redirect("/login");
-    //     return;
-    // }
+    if(!req.session.loggedIn) {
+        res.redirect("/login");
+        return;
+    }
     res.render('view-clothes');
 });
 
